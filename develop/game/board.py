6857fa7678ace40e88d028903465813fd2adc145
Yangchi <chenade0312@gmail.com>
2024-11-15 18:11:15 +0100

    create playing mode and timer system

--- develop/game/board.py
+++ develop/game/board.py
@@ -6,64 +6,91 @@ SIZE = 80
 class Board:
     def __init__(self, setting):
         self.setting = setting
-        self.w_h = SIZE * (COL + 1)
-        self.panel_x = SIZE * (COL + 3)
+        self.w_h = SIZE * (COL)
+        self.panel_x = SIZE * (COL + 2)
         self.screen = pygame.display.set_mode((2100, self.w_h + SIZE))
         self.screen.fill(self.setting.palette("board"))
 
     def draw_board(self, x=SIZE*COL + SIZE, y=SIZE, w=125, h=SIZE):
-
+        self.screen.fill(self.setting.palette("board"))
         # Draw board
-        for i in range(1, COL + 2):
+        for i in range(1, COL + 1):
             pygame.draw.line(self.screen, self.setting.palette("black"),
                              [SIZE * i, SIZE], [SIZE * i, self.w_h], 2)
             pygame.draw.line(self.screen, self.setting.palette("black"),
                              [SIZE, SIZE * i], [self.w_h, SIZE * i], 2)
         pygame.draw.circle(self.screen, self.setting.palette("black"), [SIZE * 8, SIZE * 8], 8)
 
-    def draw_player(self, play_order):
-        if play_order is None:
-            self.text_draw("PLAYER 1",self.panel_x + 120, self.w_h // 2 - 100, (100, 100, 100), COL)
-            self.text_draw("PLAYER 2",self.panel_x + 120, self.w_h // 2 + 100, self.setting.palette("black"), COL)
-        elif play_order:
-            self.text_draw("PLAYER 1",self.panel_x + 120, self.w_h // 2 - 100, self.setting.palette("red"), COL)
-            self.text_draw("PLAYER 2",self.panel_x + 120, self.w_h // 2 + 100, self.setting.palette("black"), COL)
-        elif not play_order:
-            self.text_draw("PLAYER 1",self.panel_x + 120, self.w_h // 2 - 100, self.setting.palette("black"), COL)
-            self.text_draw("PLAYER 2",self.panel_x + 120, self.w_h // 2 + 100, self.setting.palette("red"), COL)
+    def draw_player(self, g_type, play_order):
+        if g_type == "PvP":
+            if play_order:
+                self.text_draw("PLAYER 1",self.panel_x + 120, self.w_h // 2 - 100, self.setting.palette("red"), SIZE // 2)
+                self.text_draw("PLAYER 2",self.panel_x + 120, self.w_h // 2 + 100, self.setting.palette("black"), SIZE // 2)
+            elif not play_order:
+                self.text_draw("PLAYER 1",self.panel_x + 120, self.w_h // 2 - 100, self.setting.palette("black"), SIZE // 2)
+                self.text_draw("PLAYER 2",self.panel_x + 120, self.w_h // 2 + 100, self.setting.palette("red"), SIZE // 2)
+        else:
+            if play_order:
+                self.text_draw("PLAYER",self.panel_x + 120, self.w_h // 2 - 100, self.setting.palette("red"), SIZE // 2)
+                self.text_draw("COMPUTER",self.panel_x + 120, self.w_h // 2 + 100, self.setting.palette("black"), SIZE // 2)
+            elif not play_order:
+                self.text_draw("PLAYER",self.panel_x + 120, self.w_h // 2 - 100, self.setting.palette("black"), SIZE // 2)
+                self.text_draw("COMPUTER",self.panel_x + 120, self.w_h // 2 + 100, self.setting.palette("red"), SIZE // 2)
             
     def draw_score(self, player1_score, player2_score):
         self.player1_score, self.player2_score = player1_score, player2_score
-        pygame.draw.circle(self.screen, self.setting.palette("white"), (self.panel_x, self.w_h // 2 - 50), SIZE // 5)
+        pygame.draw.circle(self.screen, self.setting.palette("black"), (self.panel_x, self.w_h // 2 - 50), SIZE // 5)
         self.text_draw(str(self.player1_score),self.panel_x + 120, self.w_h // 2 - 40, (100, 100, 100), SIZE)
-        pygame.draw.circle(self.screen, self.setting.palette("black"), (self.panel_x, self.w_h // 2 + 150), SIZE//5)
+        pygame.draw.circle(self.screen, self.setting.palette("white"), (self.panel_x, self.w_h // 2 + 150), SIZE//5)
         self.text_draw(str(self.player2_score),self.panel_x + 120, self.w_h // 2 + 160, self.setting.palette("black"), SIZE)
 
-    def draw_button(self, x=SIZE*COL, y=SIZE, w=SIZE * 3, h=SIZE):
+    def click_button(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        loc = (self.x, self.y + SIZE)
+        if self.w + self.x > mouse_x > self.x and self.y + self.h > mouse_y > self.y:
+            return "start_pvp"
+
+        if self.w + self.x > mouse_x > self.x and self.y + SIZE * 2 + self.h > mouse_y > self.y + SIZE * 2:
+            return "start_pvc"
+
+        if self.w + self.x > mouse_x > self.x and self.w_h - 90 + self.h > mouse_y > self.w_h - 90:
+            return "quit"
+
+    def draw_title(self):
+        #  _____               _       
+        # |   __|___ _____ ___| |_ _ _ 
+        # |  |  | . |     | . | '_| | |
+        # |_____|___|_|_|_|___|_,_|___|
+        self.text_draw("GOMOKU", self.panel_x + 120, SIZE, self.setting.palette("black"), SIZE)
+
+    def draw_button(self, x=SIZE*COL, y=SIZE * 2, w=SIZE * 3, h=SIZE):
         self.x, self.y, self.w, self.h = self.panel_x, y, w, h
         self.button_color = self.setting.palette("button")
         self.ac_button_color = self.setting.palette("ac_button")
 
-        mouse = pygame.mouse.get_pos()
+        # Play against player.
+        loc = (self.x, self.y)
+        pygame.draw.rect(self.screen, self.button_color, (loc[0], loc[1], w, h))
+        self.text_draw("PvP", self.x + 120, loc[1] + 45, self.setting.palette("red"), COL * 2)
+        if self.click_button() == "start_pvp":
+            pygame.draw.rect(self.screen, self.ac_button_color, (loc[0], loc[1], self.w, self.h))
+            self.text_draw("START", self.x + 120, loc[1] + 45, self.setting.palette("green"), COL * 2)
 
-        # New game.
-        pygame.draw.rect(self.screen, self.button_color, (self.x, self.y, w, h))
-        self.text_draw("NEW GAME", self.x + 120, self.y + 40, (200, 0, 0), COL * 2)
-        if self.w + self.x > mouse[0] > self.x and \
-                self.y + self.h > mouse[1] > self.y:
-            pygame.draw.rect(self.screen, self.ac_button_color, (self.x, self.y, self.w, self.h))
-            self.text_draw("START", self.x + 120, self.y + 40, self.setting.palette("red"), COL * 2)
+        # Play against computer.
+        loc = (self.x, self.y + SIZE * 2)
+        pygame.draw.rect(self.screen, self.button_color, (loc[0], loc[1] , w, h))
+        self.text_draw("COMPUTER", self.x + 120, loc[1] + 45, self.setting.palette("red"), COL * 2)
+        if self.click_button() == "start_pvc":
+            pygame.draw.rect(self.screen, self.ac_button_color, (loc[0], loc[1], self.w, self.h))
+            self.text_draw("START", self.x + 120, loc[1] + 45, self.setting.palette("green"), COL * 2)
 
         # Quit.
         pygame.draw.rect(self.screen, self.button_color, (self.x, self.w_h - 90, w, h))
         self.text_draw("QUIT", self.x + 120, self.w_h - 50, (200, 0, 200), COL * 2)
-        if self.w + self.x > mouse[0] > self.x and \
-                self.w_h - 90 + self.h > mouse[1] > self.w_h - 90:
+        if self.click_button() == "quit":
             pygame.draw.rect(self.screen, self.ac_button_color, (self.x, self.w_h - 90, self.w, self.h))
             self.text_draw("Quit", self.x + 120, self.w_h - 50, (225, 0, 225), COL * 2)
-            if pygame.mouse.get_pressed()[0] == 1:
-                pygame.quit()
-                quit()
 
     def text_draw(self, text, x_pos, y_pos, font_color, font_size):
         self.text = text
@@ -78,36 +105,56 @@ class Board:
         textSurface = font.render(text, True, font_color)
         return textSurface, textSurface.get_rect()
 
-    def get_pos(self):
-        self.x_stone, self.y_stone = pygame.mouse.get_pos()
-
-        return self.x_stone, self.y_stone
-
     def get_stone_pos(self):
-        if self.x_stone % SIZE > SIZE // 2:
-            self.x_stone = (self.x_stone - self.x_stone % SIZE) + SIZE
+
+        x_stone, y_stone = pygame.mouse.get_pos()
+
+        if SIZE < x_stone < SIZE * COL and SIZE < y_stone < SIZE * COL:
+            if x_stone % SIZE > SIZE // 2:
+                x_stone = (x_stone - x_stone % SIZE) + SIZE
+            else:
+                x_stone -= x_stone % SIZE
+
+            if y_stone % SIZE > SIZE // 2:
+                y_stone = (y_stone - y_stone % SIZE) + SIZE
+            else:
+                y_stone -= y_stone % SIZE
+
+            return x_stone // SIZE - 1, y_stone // SIZE - 1
+
         else:
-            self.x_stone -= self.x_stone % SIZE
+            return None, None
 
-        if self.y_stone % SIZE > SIZE // 2:
-            self.y_stone = (self.y_stone - self.y_stone % SIZE) + SIZE
+    def draw_stone(self, play_order, x_stone, y_stone):
+        if play_order:
+            self.stone_color = self.setting.palette("black")
         else:
-            self.y_stone -= self.y_stone % SIZE
+            self.stone_color = self.setting.palette("white")
 
-        return self.x_stone, self.y_stone
+        self.x_stone, self.y_stone = x_stone, y_stone
+        loc = ((self.x_stone + 1) * SIZE, (self.y_stone + 1) * SIZE)
+        pygame.draw.circle(self.screen, self.stone_color, loc, SIZE // 2)
 
-    def draw_stone(self, stones, color_name, stone_color, x_stone, y_stone):
-        self.stones, player_color = stones, color_name
-        self.stone_color, self.x_stone, self.y_stone = stone_color, x_stone, y_stone
-        pygame.draw.circle(self.screen, self.stone_color,(self.x_stone, self.y_stone), SIZE//2)
-        self.stones[self.x_stone // SIZE - 1, self.y_stone // SIZE - 1] = (color_name == "black") and 1 or 2
-        return self.stones
+    def draw_result(self, g_type, play_order, text):
 
-    def draw_result(self, winner, text):
-        if winner == "white":
-            self.text_draw(text, self.panel_x - 20, self.w_h // 2 - 120, self.setting.palette("green"), SIZE)
+        if text == "DRAW":
+            self.text_draw("DRAW", self.panel_x + 100, self.w_h - SIZE * 4, self.setting.palette("green"), SIZE)
+            return None
 
-        elif winner == "black":
-            self.text_draw(text, self.panel_x - 20, self.w_h // 2 + 120, self.setting.palette("green"), SIZE)    
+        if g_type == "PvP":
+            if play_order == False:
+                self.text_draw(f"Player1 {text}", self.panel_x + 100, self.w_h - SIZE * 4, self.setting.palette("green"), SIZE)
+            elif play_order == True:
+                self.text_draw(f"Player2 {text}", self.panel_x + 100, self.w_h - SIZE * 4, self.setting.palette("green"), SIZE)  
 
-        return None
+        elif g_type == "PvC":
+            if play_order == False:
+                self.text_draw(f"Player {text}", self.panel_x + 100, self.w_h - SIZE * 4, self.setting.palette("green"), SIZE)
+            elif play_order == True:
+                self.text_draw(f"Computer {text}", self.panel_x + 150, self.w_h - SIZE * 4, self.setting.palette("green"), SIZE - 10)
+    
+    def draw_timer(self, turn_start_time, play_order):
+        timer_area = pygame.Rect(self.panel_x + 10, self.y + SIZE * 4, 200, 50) 
+        pygame.draw.rect(self.screen, self.setting.palette("white"), timer_area)
+        elapsed_time = (pygame.time.get_ticks() - turn_start_time)
+        self.text_draw(f"Timer: {elapsed_time / 1000:.1f}s", self.panel_x + 110,  self.y + SIZE * 4 + 30, self.setting.palette("blue"), 30)
--- develop/game/play.py
+++ develop/game/play.py
@@ -1,47 +1,67 @@
 import pygame  
-
+import random
 
 COL = 19
 SIZE = 80
 
 class Gomoku:
 
-    def __init__(self, board):
-        self.board = board
-        self.setting = self.board.setting
+    def __init__(self):
+        self.type = None
+        # self.board = board
+        # self.setting = self.board.setting
 
-    def new_game(self):
-        self.board.draw_board()
-        self.board.draw_score(0, 0)
+    def new_game(self, board, g_type):
+        self.g_type = g_type
+        board.draw_board()
+        board.draw_score(0, 0)
+        self.g_type = g_type
         
         self._stones = {}
         for i in range(COL):
             for j in range(COL):
                 self._stones[i, j] = 0
-        return self._stones, True
+        self.play_order = True
+        return self._stones, self.play_order
 
     def check_win(self, _stones):
         self.result = False
         return self.result
 
+    def check_draw(self, _stones):
+        self.result = False
+        for i in range(COL):
+            for j in range(COL):
+                if _stones[i, j] == 0:
+                    return self.result
+        self.result = True
+        return self.result
+
     # def check_legal(self, stone, color_name):
 
-    def move(self, stones, color_name, player_score, play_order):
-        self.stones, player_color, self.player_score = stones, color_name, player_score
-        self.play_order = play_order
+    def move(self, board, stones, play_order, player1_score, player2_score):
+        self.stones, self.play_order = stones, play_order
+        self.player1_score, self.player2_score = player1_score, player2_score
         self.result = None
+
+        check_draw = self.check_draw(self.stones)
+        if check_draw:
+            self.result = True
+            self.play_order = None
+            board.draw_result(self.g_type, self.play_order, "DRAW")
+            return self.player1_score, self.player2_score, None
 
         check_win = self.check_win(self.stones)
         if check_win:
             self.result = True
             self.play_order = None
             self.player_score += 1
-            self.board.draw_result(color_name, "WIN")
-            return self.player_score, self.play_order
+            board.draw_result(self.g_type, self.play_order, "WIN")
+            return self.player1_score, self.player2_score, None
            
         self.print_stones()
         self.play_order = not self.play_order
-        return self.player_score, self.play_order
+        return self.player1_score, self.player2_score, self.play_order
 
     def print_stones(self):
         for i in range(COL):
@@ -49,3 +69,13 @@ class Gomoku:
                 print(self.stones[i, j], end=" ")
             print()
         print()
+
+    def computer_move(self, stones):
+        self.stones = stones
+        # pygame.time.delay(1000)
+        while True:
+            self.x = random.randint(0, COL - 1)
+            self.y = random.randint(0, COL - 1)
+            if self.stones[self.y, self.x] == 0:
+                self.stones[self.y, self.x] = 2
+                return self.x, self.y

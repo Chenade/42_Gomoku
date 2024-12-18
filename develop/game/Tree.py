012c4ad8047d7f4104d6ef706559124db4579ce2
Lala-pukuchan <ruruover1105@gmail.com>
2024-12-18 18:52:39 +0900

    class

--- develop/game/Tree.py
+++ develop/game/Tree.py
@@ -1,4 +1,4 @@
-from Node import Node
+from game.Node import Node
 
 
 class Tree:
--- develop/game/__init__.py
+++ develop/game/__init__.py
@@ -1,3 +1,5 @@
 from .board import Board
 from .play import Gomoku
 from .AIPlayer import AIPlayer
+from .Node import Node
+from .Tree import Tree
